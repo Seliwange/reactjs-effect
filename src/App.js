ac31e7296fd3be8612a469ec5646b5472ea7f579
Seliwange <yul.yaremchuk@gmail.com>
2021-11-26 23:20:09 +0200

    feat: add homework for task reactjs useEffect hook

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Timer from './components/Timer/Timer';
+
+function App() {
+  let time = 70;
+  let step = 1000;
+
+  return (
+    <div className="App">
+        <Timer 
+          time={time} 
+          step={step}
+          onTick={(time) => console.log("Залишилось часу: " + time)}
+          onTimeEnd={() => console.log("Час вийшов!")}
+          onTimeStart={(timeLeft) => console.log("Таймер запущено!")}
+          onTimePause={(timeLeft) => console.log("Таймер на паузі!")}
+        />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,65 @@
+import './timer.css';
+import { useState, useEffect } from 'react';
+
+export default function Timer({time, step, onTick, onTimeEnd, onTimeStart, onTimePause}){
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    const [countSeconds, setCountSeconds] = useState(seconds);
+    const [countMinutes, setCountMinutes] = useState(minutes);
+    const [paused, setPaused] = useState(false);
+
+    useEffect(() => {
+      let interval = null;
+      if(paused){
+        interval = setInterval(() => {
+          countSeconds > 0 && setCountSeconds(count => count - 1);
+        }, step);
+      } 
+      return () => {
+        clearInterval(interval);
+      }
+    }, [paused, countSeconds, countMinutes]);
+
+    useEffect(() => {
+      let interval = null;
+      if(paused){
+        onTick(countMinutes + ":" + countSeconds)
+        interval = setInterval(() => {
+          countMinutes > 0 && setCountMinutes(count => count - 1);
+          if (countMinutes > 0 || (countMinutes === 0 && !countSeconds === 0)){
+            setCountSeconds(59);
+          }
+        }, step * countSeconds);
+      }
+      
+      return () => {
+        clearInterval(interval);
+      }
+    }, [paused, countMinutes, countSeconds]);
+    
+    if(countMinutes === 0 && countSeconds === 0){
+      onTimeEnd();
+    }
+    
+    function toggle(){
+      setPaused(!paused);
+      if(paused){
+        onTimePause();
+      } else {
+        onTimeStart();
+      }
+    }
+
+    return (
+      <div className="timer">
+        <h4>
+          Counter: {countMinutes < 10 ? `0${countMinutes}` : countMinutes} : {countSeconds < 10 ? `0${countSeconds}` : countSeconds}
+        </h4>
+
+        <button onClick={toggle}>
+          {paused ? "Pause" : "Start"}
+        </button>
+      </div>
+    );
+}
